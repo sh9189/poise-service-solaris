d056195c9c18b34d93396e315c06153e9d0cde27
Shahul Hameed <skhajamohid1@bloomberg.net>
2015-10-22 15:25:20 -0400

    Use solaris manifest file,add license

--- lib/poise_service/solaris/provider.rb
+++ lib/poise_service/solaris/provider.rb
@@ -19,6 +19,7 @@ module PoiseService
       include Chef::Mixin::ShellOut
       provides(:solaris_service)
 
+
       def self.provides_auto?(node, _)
         node['platform_family'] == 'solaris2'
       end
@@ -32,12 +33,23 @@ module PoiseService
 
       private
 
+      def manifest_file
+        "/lib/svc/manifest/site/#{new_resource.service_name}.xml"
+      end
+
       def create_service
         Chef::Log.debug("Creating solaris service #{new_resource.service_name}")
 
-        template "/lib/svc/manifest/site/#{new_resource.service_name}.xml" do
+        template manifest_file do
           source 'manifest.xml.erb'
+          cookbook 'poise-service-solaris' #poise needs cookbook name for template
           verify 'svccfg validate %{file}'
+          variables(
+            name: new_resource.service_name,
+            command: new_resource.command,
+            user: new_resource.user,
+            environment: new_resource.environment
+          )
           notifies :run, 'execute[load service manifest]', :immediately
         end
 
@@ -49,8 +61,7 @@ module PoiseService
 
       def destroy_service
         Chef::Log.debug("Destroying solaris service #{new_resource.service_name}")
-
-        file "/lib/svc/manifest/site/#{new_resource.service_name}.xml" do
+        file manifest_file do
           action :delete
           notifies :run, 'execute[load service manifest]', :immediately
         end
